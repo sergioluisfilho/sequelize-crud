168c2ed019e231ca952eeb63aaad665bf5b30d8d
sergi <sergi@DESKTOP-N0G59L2.local>
2021-12-21 13:41:28 -0300

    connect database and create user model

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port: any = process.env.PORT || 3000;
+
+const port: number = +process.env.PORT;
 
 app.listen(port, () => {
   console.log(`listening on port ${process.env.port} at ` + Date());
